c8342d0c1bc3cb74a8fdb75ad54f62708f37c0d0
korea8378 <korea8378@naver.com>
2020-04-22 23:58:37 +0900

    [MASTER][ADD] 3.5까지 JS실습

---
+++ Node/Node.js 교과서/실습/3장/path.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+const string = __filename;
+
+console.log('path.sep:', path.sep);
+console.log('path.delimiter:', path.delimiter);
+console.log('------------------------------');
+console.log('path.dirname():', path.dirname(string));
+console.log('path.extname():', path.extname(string));
+console.log('path.basename():', path.basename(string));
+console.log('path.basename():', path.basename(string, path.extname(string)));
+console.log('------------------------------');
+console.log('path.parse()', path.parse(string));
+console.log('path.format():', path.format({
+  dir: 'C:\\users\\leedongjun\\git\\TIL\\Node\\Node.js 교과서\\실습\\3장',
+  name: 'path',
+  ext: '.js',
+}));
+console.log('path.normalize():', path.normalize('C://users\\\\leedongjun\\\git\\\TIL\\\Node\\\Node.js 교과서\\\실습\\\3장\\\path.js'));
+console.log('------------------------------');
+console.log('path.isAbsolute():', path.isAbsolute('C:\\'));
+console.log('path.isAbsolute():', path.isAbsolute('./home'));
+console.log('------------------------------');
+console.log('path.relative():', path.relative('C:\\users\\leedongjun\\git\\TIL\\Node\\Node.js 교과서\\실습\\3장\\path.js', 'C:\\'));
+console.log('path.join():', path.join(__dirname, '..', '..', '/users', '.', '/leedongjun'));
+console.log('path.resolve():', path.resolve(__dirname, '..', 'users', '.', '/leedongjun'));
