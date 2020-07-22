7605a505ec7ab41383efedbe4f321c278a2e9dfd
korea8378 <korea8378@naver.com>
2020-07-22 22:15:12 +0900

    [MASTER][ADD] TCPvsUDP.md

--- Node/Node.js 교과서/실습/2장/promise.js
+++ Node/Node.js 교과서/실습/2장/promise.js
@@ -1,23 +1,39 @@
-function sum(a, b) {
-  return new Promise((resolve, reject) => {
-    var sum = a + b
-    if(sum < 0) {
-      reject(sum)
-    } else {
-      resolve(sum)
-    }
-  })
-}
+// function sum(a, b) {
+//   return new Promise((resolve, reject) => {
+//     var sum = a + b
+//     if(sum < 0) {
+//       reject(sum)
+//     } else {
+//       resolve(sum)
+//     }
+//   })
+// }
 
-sum(5,3)
-  .then((result) => {
-    console.log(result)
-    return result
-  })
-  .then((sum) => {
-    console.log(sum * sum)
-    return sum * sum
-  })
-  .catch((result) => {
-    console.log(result)
-  })
+// sum(5,3)
+//   .then((result) => {
+//     console.log(result)
+//     return result
+//   })
+//   .then((sum) => {
+//     console.log(sum * sum)
+//     return sum * sum
+//   })
+//   .catch((result) => {
+//     console.log(result)
+//   })
+const crypto = require('crypto');
+
+const encryption = (password => {
+	crypto.randomBytes(10, (err, buf) => {
+    const salt = buf.toString('base64')
+    
+    console.log( salt );
+		crypto.pbkdf2(password, salt, 100, 64, 'sha512', (err, key) =>{
+      console.log( "");
+      console.log( key.toString('base64'));
+      
+		})
+	})
+})
+
+encryption("mashup_recuriting");
